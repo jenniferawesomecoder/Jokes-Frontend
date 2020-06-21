fedf3e5205b79821f666b5c8db004581b6020e4d
jenniferawesomecoder <dogdoglovernumerouno1985@gmail.com>
2020-06-21 13:10:55 +0530

    Updates

---
+++ precache-manifest.9cc42f488b94f349688ce0c2703bd107.js
@@ -0,0 +1,94 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "629d7250718f2ef85f667f433753b6d4",
+    "url": "/Jokes-Frontend/index.html"
+  },
+  {
+    "revision": "c175b646dac10a3b283b",
+    "url": "/Jokes-Frontend/static/css/2.4c3b3e00.chunk.css"
+  },
+  {
+    "revision": "2dc0a0f6f281cbb05d3c",
+    "url": "/Jokes-Frontend/static/css/main.753deb97.chunk.css"
+  },
+  {
+    "revision": "c175b646dac10a3b283b",
+    "url": "/Jokes-Frontend/static/js/2.b153a005.chunk.js"
+  },
+  {
+    "revision": "2dc0a0f6f281cbb05d3c",
+    "url": "/Jokes-Frontend/static/js/main.cd9a245b.chunk.js"
+  },
+  {
+    "revision": "f79cee62bbbf825124db",
+    "url": "/Jokes-Frontend/static/js/runtime~main.d39a8d95.js"
+  },
+  {
+    "revision": "13db00b7a34fee4d819ab7f9838cc428",
+    "url": "/Jokes-Frontend/static/media/brand-icons.13db00b7.eot"
+  },
+  {
+    "revision": "a046592bac8f2fd96e994733faf3858c",
+    "url": "/Jokes-Frontend/static/media/brand-icons.a046592b.woff"
+  },
+  {
+    "revision": "a1a749e89f578a49306ec2b055c073da",
+    "url": "/Jokes-Frontend/static/media/brand-icons.a1a749e8.svg"
+  },
+  {
+    "revision": "c5ebe0b32dc1b5cc449a76c4204d13bb",
+    "url": "/Jokes-Frontend/static/media/brand-icons.c5ebe0b3.ttf"
+  },
+  {
+    "revision": "e8c322de9658cbeb8a774b6624167c2c",
+    "url": "/Jokes-Frontend/static/media/brand-icons.e8c322de.woff2"
+  },
+  {
+    "revision": "9c74e172f87984c48ddf5c8108cabe67",
+    "url": "/Jokes-Frontend/static/media/flags.9c74e172.png"
+  },
+  {
+    "revision": "0ab54153eeeca0ce03978cc463b257f7",
+    "url": "/Jokes-Frontend/static/media/icons.0ab54153.woff2"
+  },
+  {
+    "revision": "8e3c7f5520f5ae906c6cf6d7f3ddcd19",
+    "url": "/Jokes-Frontend/static/media/icons.8e3c7f55.eot"
+  },
+  {
+    "revision": "962a1bf31c081691065fe333d9fa8105",
+    "url": "/Jokes-Frontend/static/media/icons.962a1bf3.svg"
+  },
+  {
+    "revision": "b87b9ba532ace76ae9f6edfe9f72ded2",
+    "url": "/Jokes-Frontend/static/media/icons.b87b9ba5.ttf"
+  },
+  {
+    "revision": "faff92145777a3cbaf8e7367b4807987",
+    "url": "/Jokes-Frontend/static/media/icons.faff9214.woff"
+  },
+  {
+    "revision": "ada2c65324bee0dfc1745654ffa0a6e7",
+    "url": "/Jokes-Frontend/static/media/laugh.ada2c653.png"
+  },
+  {
+    "revision": "701ae6abd4719e9c2ada3535a497b341",
+    "url": "/Jokes-Frontend/static/media/outline-icons.701ae6ab.eot"
+  },
+  {
+    "revision": "82f60bd0b94a1ed68b1e6e309ce2e8c3",
+    "url": "/Jokes-Frontend/static/media/outline-icons.82f60bd0.svg"
+  },
+  {
+    "revision": "ad97afd3337e8cda302d10ff5a4026b8",
+    "url": "/Jokes-Frontend/static/media/outline-icons.ad97afd3.ttf"
+  },
+  {
+    "revision": "cd6c777f1945164224dee082abaea03a",
+    "url": "/Jokes-Frontend/static/media/outline-icons.cd6c777f.woff2"
+  },
+  {
+    "revision": "ef60a4f6c25ef7f39f2d25a748dbecfe",
+    "url": "/Jokes-Frontend/static/media/outline-icons.ef60a4f6.woff"
+  }
+]);
